1dba62c035c725904b18bc6bce7b8d562945e8fd
bblaaire <pmingoranceortega@gmail.com>
2025-04-02 14:37:09 +0200

    first commit

---
+++ Assets/Scripts/MovPersonaje.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovPersonaje : MonoBehaviour
+{
+
+    public float multiplicador = 5f;
+
+    public float multiplicadorSalto = 5f;
+
+    private bool puedoSaltar = true;
+
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        transform.position = new Vector3(-7.1f, -1.0f, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        //movimiento
+        float movTeclas = Input.GetAxis("Horizontal");
+
+        float miDeltaTime = Time.deltaTime;
+
+        Debug.Log(Time.deltaTime);
+
+       // transform.Translate(
+          // movTeclas*(Time.deltaTime*multiplicador),
+           // 0,
+          //  0
+        //);
+
+        rb.velocity = new Vector2(movTeclas*multiplicador, rb.velocity.y);
+
+        //flip
+        
+        if(movTeclas < 0){
+            this.GetComponent<SpriteRenderer>().flipX = true;
+        }else if (movTeclas > 0){
+            this.GetComponent<SpriteRenderer>().flipX = false;
+        }
+
+
+
+
+
+
+        //salto
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 0.5f);
+        Debug.DrawRay(transform.position, Vector2.down, Color.magenta);
+
+        
+        if(hit){
+            puedoSaltar = true;
+            Debug.Log(hit.collider.name);
+        }else{
+            puedoSaltar = false;
+        }
+
+
+    
+        if(Input.GetKeyDown(KeyCode.Space) && puedoSaltar){
+            rb.AddForce(
+                new Vector2(0,multiplicadorSalto), 
+                ForceMode2D.Impulse
+            );
+            //puedoSaltar = false;
+        }
+    }
+    //void OnCollisionEnter2D(){
+        //puedoSaltar = true;
+        //Debug.Log("Collision");
+        //}
+}
