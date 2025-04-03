4bed93135048b8b05922173a87679189a8700d7c
bblaaire <pmingoranceortega@gmail.com>
2025-04-03 17:32:51 +0200

    empiezan las vidas

--- Assets/Scripts/MovPersonaje.cs
+++ Assets/Scripts/MovPersonaje.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MovPersonaje : MonoBehaviour
 {
-
+    
     public float multiplicador = 5f;
 
     public float multiplicadorSalto = 5f;
@@ -13,10 +13,14 @@ public class MovPersonaje : MonoBehaviour
 
     private Rigidbody2D rb;
 
+    private Animator animatorController;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+
+        animatorController = GetComponent<Animator>();
 
         transform.position = new Vector3(-7.1f, -1.0f, 0);
     }
@@ -40,17 +44,20 @@ public class MovPersonaje : MonoBehaviour
 
         rb.velocity = new Vector2(movTeclas*multiplicador, rb.velocity.y);
 
-        //flip
-        
+        //flip izq
         if(movTeclas < 0){
             this.GetComponent<SpriteRenderer>().flipX = true;
         }else if (movTeclas > 0){
+        //dcha
             this.GetComponent<SpriteRenderer>().flipX = false;
         }
 
-
-
-
+        //Animacion walking
+        if(movTeclas != 0){
+            animatorController.SetBool("activaCamina", true);
+        }else{
+            animatorController.SetBool("activaCamina", false);
+        }
 
 
         //salto
