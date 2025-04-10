8ea0f8b02a4effd1dd63d2981bebe0007d61ffbb
bblaaire <pmingoranceortega@gmail.com>
2025-04-10 22:13:53 +0200

    parallax y nubes de lejos solo

--- Assets/Scripts/Parallax.cs
+++ Assets/Scripts/Parallax.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Parallax : MonoBehaviour
 {
+
+    public float paralaxSpeed = 1;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,6 @@ public class Parallax : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = new Vector3(Camera.main.transform.position.x/paralaxSpeed, Camera.main.transform.position.y/paralaxSpeed, 0);
     }
 }
