15271ae34c1d090db1c5424e1f7d208be39a5f09
bblaaire <pmingoranceortega@gmail.com>
2025-04-10 16:31:50 +0200

    los fantasmas persiguen

--- Assets/Scripts/Fantasma.cs
+++ Assets/Scripts/Fantasma.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class Fantasma : MonoBehaviour
 {
-
+    Vector3 posicionInicial;
+    GameObject personaje;
+    public float velocidadFantasma = 10.0f;
     public int vidaFantasma = 10;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        posicionInicial = transform.position;
+        personaje = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
@@ -19,5 +22,25 @@ public class Fantasma : MonoBehaviour
         if(vidaFantasma <= 0){
             Destroy(this.gameObject);
         }
+    
+    //Debug.Log(personaje.transform.position.x);
+    float distancia = Vector3.Distance(transform.position, personaje.transform.position);
+    float velocidadFinal = velocidadFantasma * Time.deltaTime;
+    
+
+
+    //si la distancia es mayor de 4 no me sigue
+    if(distancia <= 4.1f){
+        //se mueve
+
+        transform.position = Vector3.MoveTowards(transform.position,personaje.transform.position, velocidadFinal);
+
+    }else{
+        
+        transform.position = Vector3.MoveTowards(transform.position, posicionInicial, velocidadFinal);
+
+    }
+
+
     }
 }
